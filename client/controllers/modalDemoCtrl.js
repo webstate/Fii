64a0a7ad082d12cb7843a5afe7696f80b38e1d7a
Nelli85 <NewLife2010>
2018-01-22 12:58:31 +0200

    Broneeringumenüüs mobiilses versioonis scrolli pole

--- client/controllers/modalDemoCtrl.js
+++ client/controllers/modalDemoCtrl.js
@@ -10,13 +10,20 @@ modalDemoCtrl.controller('modalDemoCtrl', function($scope, $rootScope, $timeout)
         $scope.rightVisible = false;
     };
 
+    $scope.close_mobile = function() {
+        $('body').css('overflow', 'scroll');
+        $scope.showSideNav = false;
+        $scope.leftVisible = false;
+        $scope.rightVisible = false;
+    }
+
     $scope.showLeft = function(e) {
         $('body').css('overflow', 'hidden');
         $scope.showSideNav = true;
         if($(e.currentTarget.childNodes[1]).hasClass('active-sandwich')){
             $scope.showSideNav = false;
             $timeout(function(){
-                $scope.close();
+                $scope.close_mobile();
                 $scope.$apply();
             }, 100);
         }
